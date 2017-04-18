4d8ec135e6b817102b18d389eb0440d82f20b08a
chenxuuu <liucx1@qq.com>
2017-04-18 15:12:48 +0800

    修正匿名bug

--- Flexlive.CQP.PluginSolution/src/Flexlive.CQP.CSharpPlugins.Demo/MyPlugin.cs
+++ Flexlive.CQP.PluginSolution/src/Flexlive.CQP.CSharpPlugins.Demo/MyPlugin.cs
@@ -301,7 +301,8 @@ namespace Flexlive.CQP.CSharpPlugins.Demo
         /// <param name="font">字体。</param>
         public override void GroupMessage(int subType, int sendTime, long fromGroup, long fromQQ, string fromAnonymous, string msg, int font)
         {
-
+            if (fromQQ == 80000000)
+                return;
             // 处理群消息。
             var groupMember = CQ.GetGroupMemberInfo(fromGroup, fromQQ);
             var me = CQ.GetGroupMemberInfo(fromGroup, 751323264);
